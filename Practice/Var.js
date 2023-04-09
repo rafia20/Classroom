6af897131eb945ecb99874a37cb67db72b457169
rafia20 <rafiasajid419@gmail.com>
2023-04-09 21:03:49 +0500

    first commit

---
+++ Practice/Var.js
@@ -0,0 +1,36 @@
+// Jason Variable 
+/**
+ * Var name doesn't comes in doule qoutes
+ * Simple object create ,by var name with property acess 
+ * simple array we access that by index
+ * simple object array and we can acess that by array name index then property inside that 
+ */
+var stdName = {
+    "name": "Ali",
+    "class": "third",
+    "age": " 19",
+
+}
+//accesing name 
+stdName.age;
+// Arrays 
+var stdAge = ["10", "20", "20"];
+stdAge[2];
+// Array of objects 
+
+var thepets = [{
+    "name": "Ali",
+    "class": "third",
+    "age": " 19",
+    "year": " 19",
+},
+{
+    "name": "hina",
+    "class": "fourth",
+    "age": " 20",
+}
+
+]
+thepets[1].name;
+thepets[0].year;
+
